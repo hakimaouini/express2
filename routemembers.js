919abeaad1f3e8599ece54daa93d4d1f81089029
hakim <kimoaouini20@gmail.com>
2022-02-24 19:17:53 +0100

    second commit

--- routemembers.js
+++ routemembers.js
@@ -3,10 +3,11 @@ const express=require('express')
 const req = require('express/lib/request')
 const members = require('./members')
 const router=express.Router()
-
+//gets all membres
 router.get('/',function(req,res){
     res.json(members)
 })
+//get single membres
 
 router.get('/:id',(req , res )=>{
     for (let i = 0 ; i< members.length ; i++){
@@ -15,12 +16,36 @@ router.get('/:id',(req , res )=>{
         }
     }
 })
-
-
-router.put('/edit/:id',(req , res )=>{
+//delete membres
+router.delete('/delete/:id',(req , res )=>{
+    for (let i = 0 ; i< members.length ; i++){
+        if(members[i].id== req.params.id){
+            members.splice(i,1)
+            res.send(members)
+            res.end()
+        }
+    }
+})
+//create membre
+router.post('/membres',(req,res)=>{
+    const member = [
+        {
+            id: 10,
+            name: "hakim",
+            email: "hakim@gmail.com",
+            status: "active",
+        }
+    ];
+    members.push(member)
+    res.status(201).json(member)
+    res.end()
+})
+//update membres
+router.put('members/:id',(req , res )=>{
     for(let i = 0 ; i<members.length ; i++){
         if(members[i].id == req.params.id){
-             members[i].name=='hhhh'
+             members[i].name.splice('ll')
+             members[i].email=='hakim@email'
              res.send(members[i].name='hakim')
              res.end()
         }
